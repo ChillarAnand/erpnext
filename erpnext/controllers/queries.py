ChillarAnand/erpnext

commit daa333702aea38ff94b4dcb60c4ad7b960a7e17f

fix(bom): escape name with wildcard character (#18164)

erpnext/controllers/queries.py
@@ -206,10 +206,11 @@ def bom(doctype, txt, searchfield, start, page_len, filters):
 			if(locate(%(_txt)s, name), locate(%(_txt)s, name), 99999),
 			idx desc, name
 		limit %(start)s, %(page_len)s """.format(
-			fcond=get_filters_cond(doctype, filters, conditions),
+			fcond=get_filters_cond(doctype, filters, conditions).replace('%', '%%'),
 			mcond=get_match_cond(doctype),
-			key=searchfield), {
-			'txt': '%' + txt + '%',
+			key=frappe.db.escape(searchfield)),
+		{
+			'txt': "%"+frappe.db.escape(txt)+"%",
 			'_txt': txt.replace("%", ""),
 			'start': start or 0,
 			'page_len': page_len or 20

commit 5dcfa53de698deb6c9390f2550fa131306e5bdba

fix(BOM): Sever Error due in bom query to new behaviour of db.escape in v12 (#18401)
fixes bug introduced by commit daa333702aea38ff94b4dcb60c4ad7b960a7e17f

erpnext/controllers/queries.py
@@ -207,10 +207,10 @@ def bom(doctype, txt, searchfield, start, page_len, filters):
 			idx desc, name
 		limit %(start)s, %(page_len)s """.format(
 			fcond=get_filters_cond(doctype, filters, conditions).replace('%', '%%'),
-			mcond=get_match_cond(doctype),
-			key=frappe.db.escape(searchfield)),
+			mcond=get_match_cond(doctype).replace('%', '%%'),
+			key=searchfield),
 		{
-			'txt': "%"+frappe.db.escape(txt)+"%",
+			'txt': '%' + txt + '%',
 			'_txt': txt.replace("%", ""),
 			'start': start or 0,
 			'page_len': page_len or 20